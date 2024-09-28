cfb49d1cee23b1120dfd4f79344f0ae343243ff0
Graeme Muller <mullsy@Graemes-M3.local>
2024-09-28 16:56:03 +0800

    Added bulk_import_reinvestments(inv) command and supporting functions.

---
+++ buffetiser_api/core/management/commands/bulk_import_investments.py
@@ -0,0 +1,62 @@
+"""
+This command takes a file containing CSV data and creates Purchases and Sales
+objects from each row. Each CB (bought) and CS (sold) is of the form:
+                 3       MP1         Megaport   @   283.0000    AUD
+    Description: number  stock code  name           price       currency
+The rows are Date, Type, Description, Name, Debit, Credit, Balance
+e.g. 1/11/2022, CB, 11 VAS @ 85.4600 AUD, Vanguard Australian Shares Index ETF, 940.06, 940.06
+     3/11/2023,	CS,	189 WEB @ 6.2900 AUD, Webjet, 1177.81, -1177.81
+"""
+import csv
+from datetime import datetime
+from django.core.management.base import BaseCommand
+from core.models import Investment, Purchase, Sale
+
+
+class Command(BaseCommand):
+    """
+    python manage.py bulk_import_investments "/Users/mullsy/Downloads/statement.csv"
+    """
+    help = "This function takes a file containing CSV data and creates Purchases and Sales" \
+        "objects from each row. Usage: " \
+        "python manage.py bulk_import_investments 'path_to_cmc_file'"
+
+    def add_arguments(self, parser):
+        parser.add_argument("file_path", type=str)
+
+    def handle(self, *args, **options):
+        # Clear the database before importing all the data.
+        Investment.objects.all().delete()
+
+        with open(options["file_path"]) as file_obj:
+            reader_obj = csv.reader(file_obj)
+            trade_count = 1 # CMC data only gives date so multiple trades on one day need to be distinguished
+            for row in reader_obj:
+                description_list = row[2].split(" ")
+                if len(description_list) > 3: # catch any blank rows
+                    units, stock_code, price_per_unit = description_list[0], description_list[1], description_list[3]
+                    investment, created = Investment.objects.get_or_create(key=f"XASX-{stock_code}")
+                    if created:
+                        investment.symbol = stock_code
+                        investment.name = row[3]
+                    if row[1] == "CB":
+                        purchase = Purchase(investment=investment,
+                                            units=units,
+                                            price_per_unit=price_per_unit,
+                                            fee=11,
+                                            date=datetime.strptime(row[0], "%d/%m/%Y"),
+                                            trade_count=trade_count,)
+                        purchase.save()
+
+                    elif row[1] == "CS":
+                        sale = Sale(investment=investment,
+                                    units=units,
+                                    price_per_unit=price_per_unit,
+                                    fee=11,
+                                    date=datetime.strptime(row[0], "%d/%m/%Y"),
+                                    trade_count=trade_count,)
+                        sale.save()
+                    investment.save()
+                trade_count += 1
+
+
---
+++ buffetiser_api/core/management/commands/bulk_import_reinvestments.py
@@ -0,0 +1,46 @@
+"""
+This command takes a file containing CSV data and creates DividendReinvestment 
+object from each row. Each row is of the form:
+                 VDHG,          16/07/2024  3
+    Description: stock code     Date        Units
+e.g. VDHG,16/07/2024,3
+"""
+import csv
+from datetime import datetime
+from django.core.management.base import BaseCommand
+from core.models import DividendReinvestment, Investment
+from core.services.history import get_total_reinvestment_units, get_total_units_held
+
+
+class Command(BaseCommand):
+    """
+    python manage.py bulk_import_reinvestments "/Users/mullsy/Downloads/reinvest.csv"
+    """
+    help = "This function takes a file containing CSV data and creates DividendReinvestment" \
+        "and DividendPayment objects from each row. Usage: " \
+        "python manage.py bulk_import_dividends 'path_to_cmc_file'"
+ 
+    def add_arguments(self, parser):
+        parser.add_argument("file_path", type=str)
+
+    def handle(self, *args, **options):
+        with open(options["file_path"]) as file_obj:
+            reader_obj = csv.reader(file_obj)
+            for row in reader_obj:
+                print(row[0])
+                investment = Investment.objects.filter(symbol=row[0]).first()
+                print(investment)
+                date_elements = row[1].split("/")
+                reinvestment_date = datetime(int(date_elements[2]), 
+                                             int(date_elements[1]), 
+                                             int(date_elements[0]))
+                # cutoff_date isn't correct but it's too much work to figure out the real cutoff date
+                # as it's not provided in 'statements'. Creating a DividendReinvestment object
+                # when it shows up will be easy as the details are available when it happens.
+                reinvest = DividendReinvestment(
+                    investment=investment,
+                    cutoff_date=reinvestment_date,
+                    reinvestment_date=reinvestment_date,
+                    units=int(row[2])
+                )
+                reinvest.save()
